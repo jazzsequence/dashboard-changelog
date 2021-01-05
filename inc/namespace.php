61625f2e9d48a2e409dbde660409d1da92ee71ef
Chris Reynolds <chris@humanmade.com>
2021-01-05 15:16:08 -0700

    start adding some api stuff

--- inc/namespace.php
+++ inc/namespace.php
@@ -12,3 +12,16 @@ use jazzsequence\DashboardChangelog\API;
 function bootstrap() {
 	API\bootstrap();
 }
+
+function api_url( string $path = '' ) {
+	$base_url = apply_filters( 'dc.api.url', '://api.github.com/repos' );
+
+	return $base_url . '/' . $path;
+}
+
+function get_data() {
+	$repository = 'jazzsequence/jazzsequence.com';
+	$response = wp_remote_request( api_url( "repos/$repository" ) );
+
+	var_dump( $response );
+}
