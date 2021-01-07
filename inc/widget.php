802e36efab0646c8c9069d394a4c5c173f973386
Chris Reynolds <chris@jazzsequence.com>
2021-01-07 16:22:07 -0700

    add a bootstrap function

--- inc/widget.php
+++ inc/widget.php
@@ -8,3 +8,7 @@
 namespace jazzsequence\DashboardChangelog\Widget;
 
 use jazzsequence\DashboardChangelog\API;
+
+function bootstrap() {
+	add_action( 'wp_dashboard_setup', __NAMESPACE__ . '\\register_dashboard_widget' );
+}
