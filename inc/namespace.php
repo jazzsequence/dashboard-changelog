f97a3d6665a136bf89592bfde5ba97a73e98c1de
Chris Reynolds <chris@jazzsequence.com>
2021-01-07 13:27:55 -0700

    add inline docs

--- inc/namespace.php
+++ inc/namespace.php
@@ -9,10 +9,16 @@ namespace jazzsequence\DashboardChangelog;
 
 use jazzsequence\DashboardChangelog\API;
 
+/**
+ * Kick it off!
+ */
 function bootstrap() {
 	add_action( 'admin_init', __NAMESPACE__ . '\\add_setting' );
 }
 
+/**
+ * Register the setting and add the field.
+ */
 function add_setting() {
 	register_setting( 'general', 'dashboard_changelog', [
 		'sanitize_callback' => 'sanitize_text_field',
@@ -27,6 +33,12 @@ function add_setting() {
 	);
 }
 
+/**
+ * Get the Dashboard Changelog options.
+ *
+ * @param string $option  The option to retrieve.
+ * @param string $default (Optional) A default value.
+ */
 function get_repository_option( $option = '', $default = '' ) {
 	$options = get_option( 'dashboard_changelog' );
 
@@ -41,6 +53,9 @@ function get_repository_option( $option = '', $default = '' ) {
 	return $options[ $option ];
 }
 
+/**
+ * Display the input field for the GitHub repository.
+ */
 function render_settings_field() {
 	$repo = get_repository_option( 'repository' );
 
