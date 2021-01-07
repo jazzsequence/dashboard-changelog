a1a008a68c9597ad98283ff81058aa86fb08385f
Chris Reynolds <chris@jazzsequence.com>
2021-01-07 16:22:18 -0700

    register the widget

--- inc/widget.php
+++ inc/widget.php
@@ -12,3 +12,15 @@ use jazzsequence\DashboardChangelog\API;
 function bootstrap() {
 	add_action( 'wp_dashboard_setup', __NAMESPACE__ . '\\register_dashboard_widget' );
 }
+
+function register_dashboard_widget() {
+	wp_add_dashboard_widget(
+		'js-dashboard-changelog',
+		API\get_code() === 200 ? sprintf( __( '%s Updates', 'js-dashboard-changelog' ), API\get_name() ) : __( 'Error in Dashboard Changelog', 'js-dashboard-changelog' ),
+		 __NAMESPACE__ . '\\render_dashboard_widget',
+		 null,
+		 null,
+		 'side',
+		 'high'
+	);
+}
