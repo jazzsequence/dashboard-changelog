1a19ca73159b7a569467a66a01ee104d38bd8c42
Chris Reynolds <chris@jazzsequence.com>
2021-01-08 09:44:17 -0700

    add a simple gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const del = require( 'del' ),
+	  gulp = require( 'gulp' ),
+	  csso = require( 'gulp-csso' ),
+	  sass = require( 'gulp-sass' ),
+	  terser = require( 'gulp-terser' );
+
+gulp.task( 'styles', () => {
+	return gulp.src( 'assets/sass/**/*.scss' )
+		.pipe( sass().on( 'error', sass.logError ) )
+		.pipe( csso() )
+		.pipe( gulp.dest( 'dist/css/' ) );
+} );
+
+gulp.task( 'clean', () => {
+	return del( [
+		'dist/css/style.css',
+	] );
+} );
+
+gulp.task( 'build', gulp.series( [ 'clean', 'styles' ] ) );
+
+gulp.task( 'watch', () => {
+	gulp.watch( 'assets/sass/**/*.scss', done => {
+		gulp.series( [ 'clean:css', 'styles' ] )( done );
+	} );
+} );
