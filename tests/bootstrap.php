139ff41e2046a80d6c5775d0e3d43519e21cee5c
Chris Reynolds <chris@jazzsequence.com>
2021-10-18 13:44:00 -0600

    update bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,25 +2,20 @@
 /**
  * PHPUnit bootstrap file
  *
- * @since   1.0.0
- * @package GC\GamesCollector
+ * @since   1.1.0
+ * @package Dashboard-Changelog
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
+// Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
 // Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
+require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
-/**
- * Manually load the plugin being tested.
- */
-function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/plugin.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+tests_add_filter( 'muplugins_loaded', function() {
+    // test set up, plugin activation, etc.
+    require dirname( __DIR__ ) . '/example-plugin.php';
+} );
 
 // Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
