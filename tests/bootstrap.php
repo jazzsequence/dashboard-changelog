bf0b6160d5bf1762d4d4cbb09c3aa440b6b8a74f
Chris Reynolds <chris@jazzsequence.com>
2021-10-18 14:30:46 -0600

    fix plugin path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,7 +14,7 @@ require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
 tests_add_filter( 'muplugins_loaded', function() {
     // test set up, plugin activation, etc.
-    require dirname( __DIR__ ) . '/example-plugin.php';
+    require dirname( __DIR__ ) . '/plugin.php';
 } );
 
 // Start up the WP testing environment.
