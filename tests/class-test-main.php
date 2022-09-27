57b16ec3fb32fbcb7c124fef41490b6a2a80874e
Chris Reynolds <chris@jazzsequence.com>
2022-09-27 10:22:39 -0600

    add namespace unit tests

--- tests/class-test-main.php
+++ tests/class-test-main.php
@@ -11,7 +11,41 @@ namespace jazzsequence\DashboardChangelog;
 use PHPUnit\Framework\TestCase;
 
 class DC_Test_Main extends TestCase {
-	function test_parsedown_enabled() {
+	public function setUp() : void {
+		parent::setUp();
+
+		$this->define_constants();
+	}
+
+	private function define_constants() {
+		$options = $this->get_options();
+
+		defined( 'JSDC_REPOSITORY' ) or define( 'JSDC_REPOSITORY', $options['repo'] );
+		defined( 'JSDC_PAT' ) or define( 'JSDC_PAT', $options['pat'] );
+	}
+
+	private function get_options() : array {
+		return [
+			'repo' => 'jazzsequence/dashboard-changelog',
+			'pat' => '1234567890',
+		];
+	}
+
+	public function test_parsedown_enabled() {
 		$this->assertTrue( parsedown_enabled() );
+	}
+
+	public function test_get_cache_expiration() {
+		$this->assertEquals( DAY_IN_SECONDS, get_cache_expiration() );
+	}
+
+	public function test_get_repository() {
+		$repo = $this->get_options()['repo'];
+		$this->assertEquals( $repo, get_repository() );
+	}
+
+	public function test_get_pat() {
+		$pat = $this->get_options()['pat'];
+		$this->assertEquals( $pat, get_pat() );
 	}
 }
