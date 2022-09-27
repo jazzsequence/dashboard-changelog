c7a1776836743047e6387f4f4766ad40903f4d0c
Chris Reynolds <chris@jazzsequence.com>
2022-09-27 10:11:04 -0600

    remove tests for non-existant functions

--- tests/class-test-main.php
+++ tests/class-test-main.php
@@ -11,20 +11,6 @@ namespace jazzsequence\DashboardChangelog;
 use PHPUnit\Framework\TestCase;
 
 class DC_Test_Main extends TestCase {
-	function test_get_vendor_dir() {
-		$this->assertEquals(
-			get_vendor_dir(),
-			dirname( __DIR__ ) . '/vendor/'
-		);
-	}
-
-	function test_composer_autoloader() {
-		$this->assertEquals(
-			composer_autoloader(),
-			dirname( __DIR__ ) . '/vendor/autoload.php'
-		);
-	}
-
 	function test_parsedown_enabled() {
 		$this->assertTrue( parsedown_enabled() );
 	}
