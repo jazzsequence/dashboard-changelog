ffd3a9ebc81680f4958d8022b75eb974e72d6ab7
Chris Reynolds <chris@jazzsequence.com>
2022-09-27 10:10:52 -0600

    update our phpunit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,13 +9,19 @@
 // Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
-// Give access to tests_add_filter() function.
-require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
+/**
+ * Bootstrap WordPress
+ */
+defined( 'ABSPATH' ) or define( 'ABSPATH', __DIR__ . '/../wordpress/' );
+defined( 'WPINC' ) or define( 'WPINC', 'wp-includes' );
+defined( 'WP_PLUGIN_DIR' ) or define( 'WP_PLUGIN_DIR', '/../../' );
+defined( 'WPMU_PLUGIN_DIR' ) or define( 'WPMU_PLUGIN_DIR', '/../../' );
+require_once ABSPATH . WPINC . '/default-constants.php';
+require_once ABSPATH . WPINC . '/formatting.php';
+require_once ABSPATH . WPINC . '/functions.php';
+require_once ABSPATH . WPINC . '/load.php';
+require_once ABSPATH . WPINC . '/plugin.php';
+wp_initial_constants();
+$wp_plugin_paths = [];
 
-tests_add_filter( 'muplugins_loaded', function() {
-    // test set up, plugin activation, etc.
-    require dirname( __DIR__ ) . '/plugin.php';
-} );
-
-// Start up the WP testing environment.
-require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
+require dirname( __DIR__ ) . '/plugin.php';
